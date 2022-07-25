1d8dbd941b565d5b04f1756438bfe771a15840a1
Bornfinder <abdulwajidqureshik@gmail.com>
2022-07-25 15:36:18 +0530

    added java file

---
+++ clocksystem1.java
@@ -0,0 +1,7 @@
+public class clocksystem1 {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+        windowsystem2 w=new windowsystem2();
+
+    }
+}
---
+++ windowsystem2.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class windowsystem2 extends JFrame
+{
+    private JLabel heading;
+
+    private JLabel clockLabel;
+
+    private Font font=new Font("",Font.BOLD,35);
+
+
+    windowsystem2()
+    {
+        super.setTitle(" My Clock");
+        super.setSize(400,400);
+        super.setLocation(300,50);
+        this.createGUI();
+        this.startClock();
+        super.setVisible(true);
+    }
+
+    public void createGUI(){
+        heading=new JLabel("My Clock");
+        clockLabel=new JLabel("clock");
+        heading.setFont(font);
+        clockLabel.setFont(font);
+
+        this.setLayout(new GridLayout(2,1));
+
+        this.add(heading);
+
+        this.add(clockLabel);
+
+
+    }
+    public void startClock()
+    {
+        Timer timer=new Timer(1000, new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                String dateTime=new Date().toString();
+//                String dateTime=new Date().toLocaleString(); // for your local time
+                Date d=new Date();
+                SimpleDateFormat sdf=new SimpleDateFormat(" y :: M :: hh: mm: ss :: a"); // for your format
+                String dateTime=sdf.format(d);
+                clockLabel.setText(dateTime);
+
+
+            }
+        });
+        timer.start();
+    }
+
+
+}
+
